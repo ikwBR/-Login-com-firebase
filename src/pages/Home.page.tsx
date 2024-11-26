3b6739f9dc38f5c7a88e1b79eb524cce51c4ba19
Igor Ichikawa <144551920+ikwBR@users.noreply.github.com>
2024-11-26 17:54:40 -0300

    Create Home.page.tsx

---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,16 @@
+import Logo from "./components/Logo";
+import Navegacao from "./components/Navegacao";
+import Principal from "./components/Principal";
+const Home = () => {
+return (
+<div>
+<header className="topo">
+<Logo />
+<Navegacao />
+</header>
+<Principal />
+
+</div>
+)
+}
+export default Home
